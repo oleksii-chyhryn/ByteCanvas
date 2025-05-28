8a8e08de3c856669b8af0ab548a059a903e87853
oleksii-chyhryn <alex.xxx.sus.main@gmail.com>
2025-05-28 13:54:50 +0300

    ms-dos intel x86 asm test

---
+++ main.asm
@@ -0,0 +1,33 @@
+org 0x100
+
+start:
+    mov ax, 0x13
+    int 0x10
+
+mainloop:
+    mov ah, 0x01
+    int 0x16
+    jnz quit
+
+    mov ax, 0x0003
+    int 0x33
+    cmp bx, 1
+    jne mainloop
+
+    mov ax, 0xA000
+    mov es, ax
+
+    mov di, dx
+    mov cx, 320
+    mul cx
+    add ax, cx
+    mov di, ax
+    mov al, 15
+    stosb
+
+    jmp mainloop
+
+quit:
+    mov ax, 0x03
+    int 0x10
+    ret
