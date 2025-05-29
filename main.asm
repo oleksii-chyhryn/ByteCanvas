5aeb579578d17ba9f0befa852e9bc748359505ee
oleksii-chyhryn <alex.xxx.sus.main@gmail.com>
2025-05-29 18:07:08 +0300

    Version with bigger erasor v.0.1.1

--- main.asm
+++ main.asm
@@ -13,11 +13,21 @@ mainloop:
     int 0x16
     jnz quit
 
+    mov ax, 0x0001
+    int 0x33
+
     mov ax, 0x0003
     int 0x33
-    cmp bx, 1
-    jne mainloop
+    test bx, 1
+    jnz draw
+    test bx, 2
+    jnz erase
 
+    jmp mainloop
+draw:
+    mov ax, 0x0002
+    int 0x33
+    shr cx, 1
     mov ax, dx          ; Y -> AX
     xor dx, dx
     mov bx, 320
@@ -32,7 +42,40 @@ mainloop:
     stosb               ; write pixel at ES:DI
 
     jmp mainloop
+erase:
+    mov ax, 0x0002
+    int 0x33
+    shr cx, 1
+    mov ax, dx          ; Y → AX
+    xor dx, dx
+    mov bx, 320
+    mul bx              ; AX = Y * 320
+    add ax, cx          ; AX = Y * 320 + X
+    mov di, ax          ; DI = pixel offset
 
+    ; — ES:DI now points at first pixel in video page —
+    mov ax, 0xA000
+    mov es, ax
+    mov al, 0x00        ; color = black (erase)
+    cld
+
+    ; clear (X, Y)
+    stosb
+
+    ; clear (X+1, Y)
+    inc di
+    stosb
+
+    ; clear (X, Y+1)
+    sub di, 1           ; back to original DI
+    add di, 320
+    stosb
+
+    ; clear (X+1, Y+1)
+    inc di
+    stosb
+
+    jmp mainloop
 quit:
     mov ax, 0x0001
     int 0x33
